3af136c1dde672d91f658c9617437efef905cc8b
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-09-19 17:21:58 +0200

    Fix compressor being used
    I missed this while rebasing.

--- website/miniconcrexit/settings.py
+++ website/miniconcrexit/settings.py
@@ -127,7 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "compressor.finders.CompressorFinder",
 )
 
 
