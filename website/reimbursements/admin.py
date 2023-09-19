bd7f5fb6946d5fdf07eed6bfe7a95d46060a7386
Dirk Doesburg <dirk.doesburg@gmail.com>
2023-09-19 10:37:21 +0200

    Add hints

--- website/reimbursements/admin.py
+++ website/reimbursements/admin.py
@@ -1,1 +1,3 @@
 # Create an admin interface for your model here.
+
+# Hint: https://docs.djangoproject.com/en/4.2/ref/contrib/admin/
--- website/reimbursements/views.py
+++ website/reimbursements/views.py
@@ -1,1 +1,12 @@
-# Write your views here. Create templates in `reimbursements/templates/reimbursements/`.
+# Hint: You'll need two views: one to create a reimbursement request, and one to view
+# your own reimbursement requests. Django provides some 'generic class-based views' that
+# can handle most logic for you.
+#
+# - https://docs.djangoproject.com/en/4.2/ref/class-based-views/generic-display/
+# - https://docs.djangoproject.com/en/4.2/ref/class-based-views/generic-editing/
+
+# You might for example use these imports (but there are many other ways):
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.urls import reverse_lazy
+# from django.views.generic import ListView, CreateView
+# from reimbursements.models import <your model>
